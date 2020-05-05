eaeed862a846422c991afcb4e17ed4e53f953238
Juli <juli@nexumlabs.com>
2020-05-05 21:39:12 +0200

    Updated names of the pictures in the bar

--- js/score.js
+++ js/score.js
@@ -2,7 +2,7 @@ function showScore() {
     ctx.font = '18px Nunito';
     ctx.fillStyle = '#82368c';
     let flag = new Image();
-    flag.src = './img/flag-points.png';
+    flag.src = './img/flag-points-bar.png';
     ctx.drawImage(flag, 30, 17, 30, 34);
     character.lives > 28 ? ctx.fillText(Math.round(background.frames / 3), 70, 40) : ctx.fillText(Math.round(background.frames / 5), 70, 40);
     let mask = new Image();
@@ -10,12 +10,12 @@ function showScore() {
     ctx.drawImage(mask, 130, 17, 40, 29);
     ctx.fillText(`${Math.ceil(character.lives / 13)} / 3`, 185, 40);
     let docFace = new Image();
-    docFace.src = './img/doc-face.png';
+    docFace.src = './img/doc-face-bar.png';
     ctx.drawImage(docFace, 250, 17, 40, 29);
     ctx.fillText(`${playerName}`, 300, 40);
     if (character.lives > 28) {
         let mask = new Image();
-        mask.src = './img/timer.png';
+        mask.src = './img/timer-bar.png';
         ctx.drawImage(mask, 605, 18, 30, 31);
         ctx.font = '55px Nunito';
         ctx.fillText(Math.round(counter / 60), 650, 55);
